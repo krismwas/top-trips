fd508824ec83e23a266a5a7ac945b07358a6a119
kris <krismwasgit@gmail.com>
2024-12-27 15:44:32 +0300

    spa links

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function HomePage() {
-  return <div>Top trip</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Top trip</h1>
+    </div>
+  );
 }
 
 export default HomePage;
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,12 @@
+import PageNav from "../components/PageNav";
+
 function Product() {
-  return <div>Pricing</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Pricing</h1>
+    </div>
+  );
 }
 
 export default Product;
